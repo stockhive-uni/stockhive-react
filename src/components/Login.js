046b707aa887c28aa04ad4b20908b02cc3bfadec
JacoMH <149403504+JacoMH@users.noreply.github.com>
2024-11-04 14:49:33 +0000

    login page changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,52 +1,52 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { logIn } from '../../lib/auth';
 
-function Login() {
+import { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from 'next/router';
+
+const LoginComponent = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isLogin, setIsLogin] = useState(true);
-  const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
+  const [loginError, setLoginError] = useState('');
   const router = useRouter();
+  const auth = getAuth();
 
-  const handleAuth = async () => {
-    setError('');
-    setLoading(true); 
-    try {
-      if (isLogin) {
-        await logIn(email, password);
-      } 
-      router.push('/dashboard');
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
+  const Login = async(e) => { //used the firedocs documentation to assist in using the provided functions: https://firebase.google.com/docs/auth/web/start
+   e.preventDefault();
+   setLoading(true);
+    
+   try {
+      console.log(email, password);
+      await signInWithEmailAndPassword(auth, email, password)
+      .then((userInformation) => {
+        setLoading(false);
+        console.log("User: ", userInformation.user);
+        router.push('/dashboard');
+
+        //fetch info from
+      })
     }
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    handleAuth();
-  };
-  
-  //make function to validate if user exists in database
+    catch (error) {
+      setLoginError(error.message); //getting a 400 error constantly no idea why
+    }
+    setLoading(false);
+  }
   
   return (
     <>
     <div className="md:w-[65%] w-[90%] m-auto">
-      <h1 className="text-5xl font-extrabold text-accent my-4 text-center">{isLogin ? 'Sign in' : 'Sign Up'}</h1>
-      <form onSubmit={handleSubmit}>
+      <h1 className="text-5xl font-extrabold text-accent my-4 text-center">Login</h1>
+      <form onSubmit={Login}>
       <input type="email" className="text-white bg-black bg-opacity-25 p-2 w-full rounded-2xl my-4" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required/>
       <input type="password" className="text-white bg-black bg-opacity-25 p-2 w-full rounded-2xl my-4" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required/>
+      <div className="text-red-500 text-center">{loginError}</div>
       <button type="submit" className="rounded-lg py-2 px-4 font-bold my-4 text-white bg-accent text-xl hover:scale-105 hover:opacity-55 transition-all"disabled={loading}>
-          {loading ? 'Loading...' : ('Login')}
+          {loading ? 'Loading...' : 'Login'}
       </button>
       </form>
-      <button className="rounded-lg underline my-4 text-accent text-xl hover:scale-105 hover:opacity-55 transition-all" onClick={() => setIsLogin(!isLogin)}>{isLogin ? 'Create an account' : 'Already have an account?'}</button>
-      </div>
+     </div>
     </>
   );
 };
 
-export default Login;
+export default LoginComponent;
