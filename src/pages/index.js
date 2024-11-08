93fd44728408c36de9fa75c4f3d792ee2625534e
JacoMH <149403504+JacoMH@users.noreply.github.com>
2024-11-08 11:35:12 +0000

    changed <a> tag to <Link> tag so app can build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Link from "next/link"; 
 
 export default function Home() {
   return(
     <div className="h-screen flex items-center justify-center">
       <div>
       <h1 className="text-center text-6xl text-white font-extrabold">Stockhive</h1>
-      <p className="text-center my-8"><a className="rounded-lg py-2 px-4 font-bold text-white bg-accent text-3xl hover:scale-105 hover:opacity-55 transition-all" href="/login">Login</a></p>
+      <p className="text-center my-8"><Link className="rounded-lg py-2 px-4 font-bold text-white bg-accent text-3xl hover:scale-105 hover:opacity-55 transition-all" href="/login">Login</Link></p>
       </div>
     </div>
   );
